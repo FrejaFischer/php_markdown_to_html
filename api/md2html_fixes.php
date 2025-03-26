6f1d3cb1e0f195aab79f68bd4b15d7cdcc56505e
Freja Fischer <frej657e@stud.kea.dk>
2025-03-26 11:37:06 +0100

    added last tests, and made a fix files

--- api/md2html_fixes.php
+++ api/md2html_fixes.php
@@ -1,17 +1,13 @@
 <?php
 
 /**
- * It converts text from basic MarkDown to HTML format.
- * It only covers the "Basic Syntax" section at https://www.markdownguide.org/cheat-sheet/:
- *      headings (3 levels), bold, italic, blockquote, orderer list, unordered list, 
- *      code, horizontal rule, link and image
+ * This is the fixed version of the basic MarkDown to HTML converter.
+ * Everything is fixed - EXCEPT multi lined coding blocks
  * 
- * @author  Arturo Mora-Rioja
- * @version 1.0.0 August 2021
- * @version 1.0.1 December 2024 Adaptation to PHP8's syntax
+ * @author  Freja Fischer Nielsen
  * 
  * @param   $code   The MarkDown text to convert to HTML
- * @return  string $text converted to HTML
+ * @return  string The text converted to HTML
  */
 function markdown2html(string $code): string {
     $html = '';
@@ -21,20 +17,58 @@ function markdown2html(string $code): string {
     $blockOngoing = false;  // It will allow tracking the last iteration of a code block
     $blockquoteOpen = false;
 
-    $code = str_replace("\n", '\r\n', $code);
-    $code = explode('\r\n', $code);
-    // $code = str_replace("\n", '\n', $code);
-    // $code = explode('\n', $code);
+    $code = str_replace("\n", '\n', $code);
+    $code = explode('\n', $code);
+    // $code = str_replace("\n", '\r\n', $code);
+    // $code = explode('\r\n', $code);
     
     foreach($code as $line) {
         $line = trim($line);
 
-        // "<" and ">" are replaced by their corresponding character entities
+        // Check for blockquote before replacing `<` and `>`
+        if (substr($line, 0, 1) === '>') {
+
+            if(!$blockquoteOpen) {
+                $html .= '<blockquote>';
+                $blockquoteOpen = true;
+            }
+
+            $html .= substr($line, 2) . '<br>';
+            continue; // Move to the next line to avoid extra processing
+        }
+
+        // If new line is not a continue of a blockquote, then close the block quote
+        if ($blockquoteOpen) {
+            // If we encounter a non-blockquote line, close the blockquote
+            $html .= '</blockquote>';
+            $blockquoteOpen = false;
+        }
+        
+        // "<" and ">" are replaced by their corresponding character entities (to avoid injection of JavaScript)
         $line = str_replace('<', '&lt;', $line);
         $line = str_replace('>', '&gt;', $line);
-
+        
         // Unordered and ordered lists replacement
-        if ($ulOpen && substr($line, 0, 2) !== '- ') {
+        if (substr($line, 0, 2) === '- ') {
+            if (!$ulOpen) {
+                $html .= '<ul>';                    
+                $ulOpen = true;
+            }
+            $html .= '<li>' . substr($line, 2) . '</li>';
+            continue;
+        }
+
+        if (isOL($line)) {
+            if (!$olOpen) {
+                $html .= '<ol>';                    
+                $olOpen = true;
+            }
+            $html .= '<li>' . substr($line, 3) . '</li>';
+            continue;
+        }
+        
+        // If new line is not a list item, then close the unordered / ordered list
+        if ($ulOpen) {
             $html .= '</ul>';
             $ulOpen = false;
         }
@@ -56,7 +90,8 @@ function markdown2html(string $code): string {
                 $codeOpen = false;
             } else {
                 if ($pos === 0) {
-                    $line = '<code>' . substr($line, $pos + 1);
+                    $line = '<code>';
+                    // $line = '<code>' . substr($line, $pos + 1);
                 } else {
                     $line = substr($line, 0, $pos) . '<code>' . substr($line, $pos + 1);
                 }
@@ -75,32 +110,15 @@ function markdown2html(string $code): string {
         $line = replaceURLs($line);
         
         if (substr($line, 0, 3) === '###') {
-            $html .= '<h3>' . substr($line, 3) . '</h3>';
+            $html .= '<h3>' . substr($line, 4) . '</h3>';
         } elseif (substr($line, 0, 2) === '##') {
-            $html .= '<h2>' . substr($line, 2) . '</h2>';
+            $html .= '<h2>' . substr($line, 3) . '</h2>';
         } elseif (substr($line, 0, 1) === '#') {
-            $html .= '<h1>' . substr($line, 1) . '</h1>';
-        } elseif (substr($line, 0, 2) === '- ') {
-            if (!$ulOpen) {
-                $html .= '<ul>';                    
-                $ulOpen = true;
-            }
-            $html .= '<li>' . substr($line, 2) . '</li>';
-        } elseif (substr($line, 0, 1) === '>') {
-            $html .= '<blockquote>' . substr($line, 1) . '<br>';
-            $blockquoteOpen = true;
-        } elseif (isOL($line)) {
-            if (!$olOpen) {
-                $html .= '<ol>';                    
-                $olOpen = true;
-            }
-            $html .= '<li>' . substr($line, 2) . '</li>';
+            $html .= '<h1>' . substr($line, 2) . '</h1>';
         } elseif ($line === '---') {
             $html .= '<hr>';
-        } elseif ($line === '' && $blockquoteOpen) {
-            $html .= '</blockquote>';
-            $blockquoteOpen = false;
         } elseif (!$blockOngoing && !$blockquoteOpen) {
+            // Normal paragraf
             $html .= '<p>' . $line . '</p>';
         } else {
             // A block of code has just been closed in this iteration
@@ -108,7 +126,18 @@ function markdown2html(string $code): string {
                 $blockOngoing = false;
             }
             $html .= $line . '<br>';
-        }
+        } 
+    }
+
+    // Close any open blockquote or unordered lists at the end
+    if ($blockquoteOpen) {
+        $html .= '</blockquote>';
+    }
+    if ($ulOpen) {
+        $html .= '</ul>';
+    }
+    if ($olOpen) {
+        $html .= '</ol>';
     }
     return $html;
 }
@@ -129,7 +158,7 @@ function isOL(string $text): bool {
  * @param   $text       The text where to perform the replacement
  * @param   $replaced   The substring to replace
  * @param   $htmlTag    The HTML tag which will replace the substring
- * @return  string text with the replacements done
+ * @return  string The text with the replacements done
  */
 function replaceByHTMLTags(string $text, string $replaced, string $htmlTag): string {
     $replacedLength = strlen($replaced);
@@ -153,7 +182,7 @@ function replaceByHTMLTags(string $text, string $replaced, string $htmlTag): str
  * It replaces markdown URLs by HTML URLs in a text
  * 
  * @param   @text   The text where to perform the replacements
- * @return  string text with the replacements done
+ * @return  string The text with the replacements done
  */
 function replaceURLs(string $text): string {
     $offset = 0;
@@ -182,7 +211,7 @@ function replaceURLs(string $text): string {
  * It replaces markdown images by HTML images in a text
  * 
  * @param   @text   The text where to perform the replacements
- * @return  string text with the replacements done
+ * @return  string The text with the replacements done
  */
 function replaceImages(string $text): string {
     $offset = 0;
--- tests/md2htmlTest.php
+++ tests/md2htmlTest.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once 'api/md2html.php';
+require_once 'api/md2html.php'; // Test original version
+// require_once 'api/md2html_fixes.php'; // Test fixed version
 
 
 use PHPUnit\Framework\TestCase;
@@ -25,13 +26,22 @@ class md2htmlTest extends TestCase
     {
         return [
             ['This is a paragraf', '<p>This is a paragraf</p>'], // Normal text
-            ['# First', '<h1>First</h1>'], // Header one
-            ['## Second', '<h2>Second</h2>'], // Header two
-            ['### Third', '<h3>Third</h3>'], // Header three
-            ['This a **Bold** test', '<p>This a <strong>Bold</strong> test</p>'], // Bold
+            // ['# First', '<h1>First</h1>'], // Header one - FAILS because of space in front
+            // ['## Second', '<h2>Second</h2>'], // Header two - FAILS because of space in front
+            // ['### Third', '<h3>Third</h3>'], // Header three - FAILS because of space in front
+            ['This is a **Bold** test', '<p>This is a <strong>Bold</strong> test</p>'], // Bold
             ['This a *Italic* test', '<p>This a <em>Italic</em> test</p>'], // Italic
-            ['> This a Blockquote', '<blockquote>This a Blockquote<br></blockquote>'], // Blockquote
-            ['> This is a quote \n > multiple lines', '<blockquote>This is a quote<br>multiple lines<br></blockquote>'], // Multi lined Blockquote
+            // ['> This a Blockquote', '<blockquote>This a Blockquote<br></blockquote>'], // Blockquote - FAILS because of character entities is being replaced before checking for > Blockquote
+            // ['> This is a quote \n > multiple lines', '<blockquote>This is a quote<br>multiple lines<br></blockquote>'], // Multi lined Blockquote - FAILS because of character entities is being replaced before checking for > Blockquote
+            // ['- PHP\n- MariaDB\n- jQuery', '<ul><li>PHP</li><li>MariaDB</li><li>jQuery</li></ul>'], // Unordered list - FAILS because of \n not being replaced and exploded correctly and missing </ul> closing tag
+            // ['1. Create the database \n 2. Copy the files to your own server \n 3. Run the app at localhost', '<ol><li>Create the database</li><li>Copy the files to your own server</li><li>Run the app at localhost</li></ol>'], // Ordered list - FAILS because of \n fail and missing </ol> closing tag
+            // ['```\n$server = "localhost";\n$user = "root";\n$pwd = "";\n```', '<code><br>$server = "localhost";<br>$user = "root";<br>$pwd = "";<br></code>'], // Code block multi lined - FAILS because of repeating starting and closing code tags in front and in the end (It does not support multi line coding blocks), and \n fails
+            // ['`\n$server = "localhost";\n$user = "root";\n$pwd = "";\n`', '<code><br>$server = "localhost";<br>$user = "root";<br>$pwd = "";<br></code><br>'], // Code block - FAILS because of \n fails
+            ['---','<hr>'], // Horizontal line
+            ['Welcome to [KEA](https://www.kea.dk).','<p>Welcome to <a href="https://www.kea.dk" target="_blank" title="KEA">KEA</a>.</p>'], // Links
+            ['First [KEA](https://www.kea.dk), second [KEA](KEA.DK)','<p>First <a href="https://www.kea.dk" target="_blank" title="KEA">KEA</a>, second <a href="KEA.DK" target="_blank" title="KEA">KEA</a></p>'], // Mulitple links
+            ['[This] is not a URL','<p>[This] is not a URL</p>'], // Not a link - Paragraph almost like a link
+            ['image: ![KEA](https://kea.dk/slir/w640-c100x60/images/efteruddannelser/webinarer-paa-kea-teaser.jpg.webp)','<p>image: <img src="https://kea.dk/slir/w640-c100x60/images/efteruddannelser/webinarer-paa-kea-teaser.jpg.webp" alt="KEA"></p>'], // Image
         ];
     }
 }
